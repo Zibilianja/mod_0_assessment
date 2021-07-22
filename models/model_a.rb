5762782bade610c9bae6e311944801a899a2d1c9
Jon Fuller <j.fuller.e@gmail.com>
2021-07-22 10:50:40 -0600

    Complete hash challenge

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+school = {
+  school_name: "Faketown High School",
+  principal: "Daniel Granule",
+  num_students: 1200,
+  num_teachers: 95,
+  is_public: true,
+  is_good_school: false,
+  subjects: ["science", "math", "language", "art", "physical education"],
+  sports: ["soccer", "football", "swimming", "track and field", "lacrosse"]
+}
