d104cd30beea4bb8c159db5b54acaf88ed3dd3c5
Jon Fuller <j.fuller.e@gmail.com>
2021-07-22 10:44:34 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,9 @@
+famous_person = "Morgan Freeman"
+is_active = true
+famous_person = "Jerry Garcia"
+num_3 = 31
+num_9 = 99
+sum = num_3 + num_9
+animals = ["goose", "squirrel", "dog"]
+animals.shift()
+p famous_person.length
