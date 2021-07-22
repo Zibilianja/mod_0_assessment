3a7f505e97222e93b95441822c80c536fbbc72f9
Jon Fuller <j.fuller.e@gmail.com>
2021-07-22 10:54:59 -0600

    Complete hash challenge B

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+pet = {
+  name: "Kogen",
+  breed: "Plott Hound and Boxer mix",
+  age: 8,
+  weight_lbs: 65.9,
+  is_good_boy: true,
+  is_rescue: true,
+  coat_colors: ["orange", "black", "white"],
+  cat_friends: ["iago", "habibi", "bug"]  
+}
